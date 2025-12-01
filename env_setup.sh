f0100ecad0524c9ba786116e5918dd6399c1cf34
zelunli <zelun.li9@gmail.com>
2025-12-01 08:53:02 +0000

    main env setup

---
+++ env_setup.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+conda config --set auto_activate_base false
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+conda create -y -n evo2_embeddings python=3.12
+conda install -c nvidia cuda-nvcc cuda-cudart-dev
+conda install -c conda-forge transformer-engine-torch=2.3.0
+pip install psutil
+pip install flash-attn==2.8.0.post2 --no-build-isolation
+pip install evo2
+pip install -e .
